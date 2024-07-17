928df992729bc2d3691ba2866eebe67e9a2646a5
HeyK20 <18236784+HeyK20@users.noreply.github.com>
2024-07-16 22:16:03 -0400

    Add depends and first gui launch

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"time"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
 func main() {
-	// TODO: implement
+	a := app.New()
+	w := a.NewWindow("Hello")
+	// Define progress bar and infinite progress bar
+	progress := widget.NewProgressBar()
+	infinite := widget.NewProgressBarInfinite()
+
+	hello := widget.NewLabel("Hello Fyne!")
+	go func() {
+		for i := 0.0; i <= 1.0; i += 0.1 {
+			time.Sleep(time.Millisecond * 1500)
+			progress.SetValue(i)
+		}
+	}()
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!",
+			func() {
+				hello.SetText("Welcome :)")
+			}),
+		progress,
+		infinite))
+
+	w.ShowAndRun()
 }
